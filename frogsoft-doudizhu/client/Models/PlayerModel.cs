6504e66999380cef77b47aa584834fb17c08ba89
QQKdeGit <627948700@qq.com>
2021-12-23 02:08:58 +0800

    feat: add model

---
+++ frogsoft-doudizhu/client/Models/PlayerModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Models
+{
+    internal class PlayerModel
+    {
+        public string Id {  get; set; } 
+        public PlayerStatus Status { get; set; } = PlayerStatus.NOT_READY;
+
+        public List<int> CardsInHand = new List<int>();  
+
+        public List<int> CardsOut = new List<int>();
+
+        //public string Avatar = "";
+        public int CallScore = -1;
+    }
+
+    public enum PlayerStatus
+    {
+        NOT_READY,
+        READY,
+        LANDLORD,
+        PEASANT
+    }
+}
